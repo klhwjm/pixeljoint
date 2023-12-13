de5016a10b4eab8d776641420f6a50218a8250a8
klhwjm <allanseixassantos123@gmail.com>
2023-12-13 18:27:13 -0300

    welp

--- example_main.py
+++ example_main.py
@@ -5,9 +5,5 @@ import requests
 session = requests.session()
 session.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.3"})
 
-# List
-with open("list.txt", "r", encoding="utf-8") as f:
-	_list = f.read().splitlines()
-
-pj = Pixeljoint("/home/avoid/pixeljoint/artists", _list, session)
+pj = Pixeljoint(path, list_, session)
 pj.start()
